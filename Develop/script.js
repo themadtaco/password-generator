c4c4b5dbd4821c9c32f8aed3f923b78b4cacd363
Drogon <aatrox48@gmail.com>
2021-12-19 22:45:55 -0600

    refactored code and finished for loop

--- Develop/script.js
+++ Develop/script.js
@@ -1,47 +1,63 @@
-//Global variables
-var genLength = 0; 
-
 var generatePassword = function(){
-  var passLength = window.prompt("How long would you like your password? Please choose between 8 to 128 characters.")
+  // Variable
+  var passwordChars = "";
+  var length = getLength();
+
+  // First prompt
+  window.alert("Please confirm whether or not you would like the following character types in your password.");
+
+  // boolean variables for password criteria
+  var passLow = window.confirm("Would you like Lowercase letters?");
+  if (passLow){
+    passwordChars += keyStrings.lower;
+  };
+
+  var passHigh = window.confirm("Would you like Uppercase letters?");
+  if (passHigh){
+    passwordChars += keyStrings.upper;
+  };
+
+  var passNumeric= window.confirm("Would you like Numeric characters?");
+  if (passNumeric){
+    passwordChars += keyStrings.numeric;
+  };
+
+  var passSpec = window.confirm("Would you like Special characters?");
+  if (passSpec){
+    passwordChars  += keyStrings.special;
+  };
+
+  // for loop that randomly add a variable to the string
+  var password = "";
+  for (var i = 0; i < length; i++){
+    password += passwordChars[Math.floor(Math.random() * passwordChars.length)];
+  };
+
+  return password;
+};
+
+// get password length and check for other inputs
+var getLength = function() {
+  var passLength = window.prompt("How long would you like your password? Please choose between 8 to 128 characters.");
 
   // check passLength input
   if (passLength >= 8 && passLength <= 128){
-    genLength = passLength;
+    console.log(passLength);
+    return passLength;
   }
   else{
     window.alert("Please input a number between 8 and 128!");
     return generatePassword();
   };
-
-  console.log(genLength);
-
-  window.alert("Please confirm whether or not you would like the following character types in your password.");
-
-  // boolean variables for password criteria
-  var passLow = window.confirm("Would you like Lowercase letters?");
-  //var passHigh = window.confirm("Would you like Uppercase letters?");
-  //var passNumeric= window.confirm("Would you like Numeric characters?");
-  // var passSpec = window.confirm("Would you like Special characters?");
-
-  if (passLow) {
-    passLow = genLower();
-  };
 };
 
-// Random generator functions
-
-var genLower = function(){
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+// Password variables object
+var keyStrings = {
+  lower: "abcdefghijklmnopqrstuvwxyz",
+  upper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  numeric: "0123456789",
+  special: "!/#$%&'()*+,-,/;;<=>?"
 };
-
-var genUpper = function(){
-  return String.fromCharCode(Math.floor(Math.random() *26) + 65);
-};
-
-var genNumber = function() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-};
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
